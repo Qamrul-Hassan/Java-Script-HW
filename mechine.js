ee30f97a37efcf53eb51be9508bb86500e5217bd
Qamrul-Hassan <sajal700@gmail.com>
2024-09-13 10:48:42 +0600

    Update mechine.js

--- mechine.js
+++ mechine.js
@@ -1,15 +1,14 @@
-let machines = 6; // Initial number of machines
-let productsPerDay = 200; // Total products produced per day
-let additionalMachines = 2; // Additional machines added
-let unsoldProducts = 3; // Products not sold
+let machines = 6;
+let productsPerDay = 200;
+let additionalMachines = 2;
+let unsoldProducts = 3;
 
-// Products produced by one machine per day
 let productsPerMachine = productsPerDay / machines;
 
-// Total products produced by 8 machines (6 + 2)
+
 let totalProduction = productsPerMachine * (machines + additionalMachines);
 
-// Final number of products after subtracting unsold products
+
 let finalProducts = totalProduction - unsoldProducts;
 let fianlproductsinhand = totalProduction - finalProducts;
 finalOutPut = "factory have only :" + ' ' + fianlproductsinhand + ' ' + 'products';
